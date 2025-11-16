80db2615afc7ace606d3334bba56a299697c7a7a
yaza <yannick.zander@googlemail.com>
2025-11-16 15:38:10 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='LipidCalculator',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/yaza11/LipidCalculator',
     author='Yannick Zander',
     author_email='yannick.zander@gmail.com',
